0f4486375fb6eca95f5997c9e7a832eea89a5c6f
Jan3r <110995928+Jan3r@users.noreply.github.com>
2022-08-31 11:11:52 -0500

    Add files via upload

---
+++ clase.js
@@ -0,0 +1,25 @@
+console.log("Hola mundo");
+
+// Declaracion de variables// 
+
+let suma;
+let resta;
+
+const printHolaDos = Mensaje =>{
+    //console.log('Hi${Mensaje}')
+    console.log(Mensaje);
+}
+
+printHolaDos('Hi');
+
+const printHolaTres = Mensaje => console.log(Mensaje);
+printHolaTres('no?')
+
+const printHolaCinco = function(){
+    console.log('hola cinco');
+}
+
+printHolaCinco()
+
+
+//document.getElementById("demo").style.display = 'block';
